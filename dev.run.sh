db589366e79589c309a463583836b4e80b27c120
diogorodriguees <diogorodrigueslife@gmail.com>
2024-07-21 21:53:26 -0300

    ci: configuring nginx

---
+++ dev.run.sh
@@ -0,0 +1,8 @@
+echo "[Docker Compose] Building app ..."
+docker compose up --build --watch
+
+echo "\n[Docker] Containers createds"
+docker ps --format "table {{.Names}}\t{{.Ports}}" | sort -k 3 | grep app-trainning
+
+echo "\n[Application] Server running in http://localhost:3000"
+echo "[Application] Postgres server running in http://localhost:5433"
